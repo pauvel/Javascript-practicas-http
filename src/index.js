e5be7c6beb86ba0706ba218e92b96aba9fdff9cc
Paul G <paulsystem32@gmail.com>
2020-06-15 01:36:54 -0600

    Subida de imagenes a servidor
    - Comenzamos a estructurar y programar la funcionalidad
      para que podamos subir imagenes a cloudinary

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-import * as CRUD from './js/crud-provider';
+import {init} from './js/archivos-page';
 
-// CRUD.getUsuario(1).then( (data) =>{
-//     console.log(data);
-// });
-
-// CRUD.crearUsuario({
-//     name: 'Paul',
-//     job: 'programmer'
-// }).then(console.log);
-
-// CRUD.actualizarUsuario(1, {
-//     name: 'Vershy',
-//     job: 'cleaner'
-// }).then(console.log);
-
-CRUD.borrarUsuario(10).then(console.log);
+init();
---
+++ src/js/archivos-page.js
@@ -0,0 +1,24 @@
+const body = document.body;
+let inputFile, imgFoto;
+
+const crearInputFileHtml = () =>{
+    const html =  `
+        <h1 class="mt-5">Subir archivos</h1>
+        <hr>
+
+        <label> Selecciona una fotografia:</label>
+        <input type="file" accept="image/png, image/jpg, image/jpeg"/>
+        <br>
+        <img id="foto" class="img-thumbnail" src=""/>
+    `;
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    body.append(div);
+    inputFile = document.querySelector('input');
+    imgFoto = document.querySelector('#foto');
+}
+
+
+export const init = ()=>{
+    crearInputFileHtml();
+}
